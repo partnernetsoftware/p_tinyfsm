da0363e1c58ed30fd0ea7b9404287c6f9154b5ca
wanjochan <wanjochan@gmail.com>
2019-09-23 10:43:27 +0800

    expose .run and .compile

--- p_tinyfsm.js
+++ p_tinyfsm.js
@@ -1,11 +1,13 @@
-module.exports = async (init_opts,init_fsm,init_step_name) => {
+var out = async (init_opts,init_fsm,init_step_name) => out.run(
+	init_fsm ? {lgc:init_opts,fsm:init_fsm,step_name:init_step_name} : init_opts,
+);
+out.compile = (fsm_s,pt,m) => fsm_s.split(/[\n\r]+/).reduce(
+	(r,e)=>(m=e.replace(/\s/g,'').match(/^(\w+)?(\.(\w*)=>(\w*))?/))&&(pt=m[1]||pt,pt&&(r[pt]=(r[pt]||{}),r[pt][m[3]]=m[4]),r),{}
+);
+out.run = async (init_opts={}) => {
 	try{
-		var {lgc,fsm,step_name} = init_fsm ? {lgc:init_opts,fsm:init_fsm,step_name:init_step_name} : (init_opts||{});
-		var prev_sts,prev_rst,prev_name,pt,m;
-		//auto parse fsm from str to obj:
-		var fsm_o = (typeof fsm=='object') ? fsm : fsm.split(/[\n\r]+/).reduce(
-			(r,e)=>(m=e.replace(/\s/g,'').match(/^(\w+)?(\.(\w*)=>(\w*))?/))&&(pt=m[1]||pt,pt&&(r[pt]=(r[pt]||{}),r[pt][m[3]]=m[4]),r),{}
-		);
+		var {lgc,fsm,step_name,prev_rst,prev_sts,prev_name} = init_opts;
+		var fsm_o = (typeof fsm=='object') ? fsm : out.compile(fsm);//console.log('fsm_o=',fsm_o);
 		if(!step_name){ for(var nm in fsm_o){ if(nm){step_name=nm; break}} }
 		do{
 			var func_p = lgc[step_name];
@@ -18,11 +20,10 @@ module.exports = async (init_opts,init_fsm,init_step_name) => {
 				step_name = step_next;
 				prev_sts = STS;
 				prev_rst = rst;
-			}else return {STS:'KO',
-				errmsg:((prev_name)?`${prev_name}.${prev_sts}`:'')+(`=>${step_name} not found?`)
-			};
+			}else return {STS:'KO', errmsg:((prev_name)?`${prev_name}.${prev_sts}`:'')+(`=>${step_name} not found?`) };
 		}while(true);
 	}catch(err){
 		return {STS:'KO',errmsg:(''+err),err};
 	}
-};
+}
+module.exports = out;
